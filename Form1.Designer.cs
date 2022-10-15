7dd438182f3c29053082826a8a844a583432dedc
ZikrullaYashinov <zikrullayashinov173@gmail.com>
2022-10-15 13:39:49 +0500

    add csharp

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBoxSlowly = new System.Windows.Forms.CheckBox();
+            this.checkBoxMidle = new System.Windows.Forms.CheckBox();
+            this.checkBoxFast = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(198)))), ((int)(((byte)(218)))));
+            this.label1.Location = new System.Drawing.Point(536, 285);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ok";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(198)))), ((int)(((byte)(218)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(140, 367);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(140, 319);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(427, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // checkBoxSlowly
+            // 
+            this.checkBoxSlowly.AutoSize = true;
+            this.checkBoxSlowly.FlatAppearance.BorderSize = 0;
+            this.checkBoxSlowly.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(198)))), ((int)(((byte)(218)))));
+            this.checkBoxSlowly.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBoxSlowly.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxSlowly.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(198)))), ((int)(((byte)(218)))));
+            this.checkBoxSlowly.Location = new System.Drawing.Point(140, 180);
+            this.checkBoxSlowly.Name = "checkBoxSlowly";
+            this.checkBoxSlowly.Size = new System.Drawing.Size(66, 23);
+            this.checkBoxSlowly.TabIndex = 4;
+            this.checkBoxSlowly.Text = "Sekin";
+            this.checkBoxSlowly.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxMidle
+            // 
+            this.checkBoxMidle.AutoSize = true;
+            this.checkBoxMidle.FlatAppearance.BorderSize = 0;
+            this.checkBoxMidle.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(198)))), ((int)(((byte)(218)))));
+            this.checkBoxMidle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBoxMidle.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxMidle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(198)))), ((int)(((byte)(218)))));
+            this.checkBoxMidle.Location = new System.Drawing.Point(140, 209);
+            this.checkBoxMidle.Name = "checkBoxMidle";
+            this.checkBoxMidle.Size = new System.Drawing.Size(62, 23);
+            this.checkBoxMidle.TabIndex = 4;
+            this.checkBoxMidle.Text = "O\'rta";
+            this.checkBoxMidle.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFast
+            // 
+            this.checkBoxFast.AutoSize = true;
+            this.checkBoxFast.FlatAppearance.BorderSize = 0;
+            this.checkBoxFast.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(198)))), ((int)(((byte)(218)))));
+            this.checkBoxFast.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBoxFast.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxFast.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(198)))), ((int)(((byte)(218)))));
+            this.checkBoxFast.Location = new System.Drawing.Point(140, 238);
+            this.checkBoxFast.Name = "checkBoxFast";
+            this.checkBoxFast.Size = new System.Drawing.Size(49, 23);
+            this.checkBoxFast.TabIndex = 4;
+            this.checkBoxFast.Text = "Tez";
+            this.checkBoxFast.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(198)))), ((int)(((byte)(218)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(266, 367);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Back";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 548);
+            this.Controls.Add(this.checkBoxFast);
+            this.Controls.Add(this.checkBoxMidle);
+            this.Controls.Add(this.checkBoxSlowly);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBoxSlowly;
+        private System.Windows.Forms.CheckBox checkBoxMidle;
+        private System.Windows.Forms.CheckBox checkBoxFast;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        int e = 0;
+        bool d=true;
+        bool next = false;
+        bool back = false;
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+            label1.Text = e + "%";
+        }
+
+        void Bar()
+        {
+            
+            int b = 0;
+            if (checkBoxSlowly.Checked)
+            {
+                b = 200;
+            }
+            else if(checkBoxMidle.Checked)
+            {
+                b = 100;
+            }
+            else if(checkBoxFast.Checked)
+            {
+                b = 10;
+            }
+            if(b != 0)
+            {
+                if (d)
+                {
+                    while (progressBar1.Value != 100)
+                    {
+                        if (!next)
+                        {
+                            break;
+                        }
+                        progressBar1.Value++;
+                        e++;
+                        label1.Text = e + "%";
+                        Thread.Sleep(b);
+                    }
+                }
+                else
+                {
+                    while (progressBar1.Value != 0)
+                    {
+                        if (!back)
+                        {
+                            break;
+                        }
+                        progressBar1.Value--;
+                        e--;
+                        label1.Text = e + "%";
+                        Thread.Sleep(b);
+                    }
+                }
+                
+            }
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread th = new Thread(Bar);
+            d = true;
+            next = true;
+            back = false;
+            th.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread th = new Thread(Bar);
+            d = false;
+            next = false;
+            back = true;
+
+            th.Start();
+        }
+    }
+}
